3cb46dbe9b945bee63ba1a3818ce61e9ba9ccbf5
Ritik <raj769417@gmail.com>
2025-07-05 00:38:15 +0530

    feat: delete fpp compoennt

---
+++ components/delete-frequent-flyer.tsx
@@ -0,0 +1,53 @@
+import { Trash } from "lucide-react";
+import { Button } from "./ui/button";
+import { useQueryClient } from "@tanstack/react-query";
+import { ClientFrequentFlyerProgram } from "@/app/_models/frequent-flyer-program.model";
+import { deleteFrequentFlyerProgram } from "@/actions";
+
+interface DeleteFrequentFlyerProps {
+  id: string;
+  queryKey: string;
+  sortField: string;
+  sortOrder: "asc" | "desc";
+  page: number;
+}
+
+export function DeleteFrequentFlyer({
+  id,
+  queryKey,
+  sortField,
+  sortOrder,
+  page,
+}: DeleteFrequentFlyerProps) {
+  const queryClient = useQueryClient();
+
+  const handleDelete = async () => {
+    // Optimistic removal
+    queryClient.setQueryData<{
+      programs: ClientFrequentFlyerProgram[];
+      total: number;
+    }>([queryKey, page, sortField, sortOrder], (old) => {
+      if (!old) return { programs: [], total: 0 };
+
+      return {
+        programs: old.programs.filter((ffp) => ffp._id !== id),
+        total: old.total - 1,
+      };
+    });
+
+    try {
+      await deleteFrequentFlyerProgram(id);
+
+      // clear complete cache
+      queryClient.invalidateQueries({ queryKey: [queryKey] });
+    } catch (err) {
+      console.error("Delete failed");
+    }
+  };
+
+  return (
+    <Button size="sm" variant="destructive" onClick={handleDelete}>
+      <Trash />
+    </Button>
+  );
+}
