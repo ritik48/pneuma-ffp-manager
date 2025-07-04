7fc393e90c9d19119ec2e118c44f29fa7d66c19e
Ritik <raj769417@gmail.com>
2025-07-04 18:27:37 +0530

    feat: protected

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { auth } from "@/auth";
+import { Logout } from "@/components/logout";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  const isAutenticated = !!session?.user;
+
+  if (!isAutenticated) {
+    redirect("/login");
+  }
+
+  console.log({ isAuthenticated: isAutenticated, session });
   return (
     <div>
-      <div>Hello world!</div>
+      <div className="max-w-7xl mx-auto">
+        <div>You are logged in {session.user?.email}</div>
+        <Logout />
+      </div>
     </div>
   );
 }
