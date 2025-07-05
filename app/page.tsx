21ba7f7e29aceb39dfa8c46a6033c5c2d3c3443a
Ritik <raj769417@gmail.com>
2025-07-05 17:57:55 +0530

    feat: added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import { auth } from "@/auth";
 import FrequentFlyerTable from "@/components/frequent-flyer-table";
+import { NavBar } from "@/components/NavBar";
 import Provider from "@/components/Provider";
+import { redirect } from "next/navigation";
 
-export default function FrequentFlyersPage() {
+export default async function FrequentFlyersPage() {
+  const session = await auth();
+  const isAuthenticated = Boolean(session?.user?.email);
+  if (!isAuthenticated) {
+    redirect("/login");
+  }
   return (
-    <div className="mt-10">
+    <div className="">
+      <NavBar email={session!.user!.email as string} />
       <div className="max-w-7xl mx-auto">
         <Provider>
           <FrequentFlyerTable />
