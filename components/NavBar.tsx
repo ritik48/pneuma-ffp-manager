b727a4a0bee20eb39e8dca9da0695705b3eed725
Ritik <raj769417@gmail.com>
2025-07-05 22:49:00 +0530

    feat: added theme change option

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { Logout } from "./logout";
+import { ModeToggle } from "./theme-toggle";
 
 export function NavBar({ email }: { email: string }) {
   return (
     <nav className="border-b mb-4">
       <div className="max-w-7xl mx-auto flex gap-4 items-center justify-end py-2 px-3">
         <div className="text-sm hidden sm:block">welcome 👋 {email}</div>
+        <ModeToggle />
         <Logout />
       </div>
     </nav>
