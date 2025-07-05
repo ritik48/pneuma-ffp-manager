313da9200998a1bd422cb323d002401f68ce4907
Ritik <raj769417@gmail.com>
2025-07-05 23:30:16 +0530

    feat: protected seed route

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,7 +1,18 @@
 import { seed } from "@/app/seed";
+import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const session = await auth();
+  if (!session?.user) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "You are not authneticated.",
+      },
+      { status: 401 }
+    );
+  }
   await seed();
 
   return NextResponse.json(
